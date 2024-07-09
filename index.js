c37b113851d161422a29e29a23ad8ee61322d3c8
CODE-INAYAT <134628417+CODE-INAYAT@users.noreply.github.com>
2024-07-09 23:11:13 +0530

    Added fetch - FinalMusic.m4a

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
             showLoading();
 
             audioContext = new (window.AudioContext || window.webkitAudioContext)();
-            fetch('https://assets.mixkit.co/sfx/preview/mixkit-light-rain-loop-2393.mp3')
+            fetch('FinalMusic.m4a')
                 .then(response => response.arrayBuffer())
                 .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
                 .then(audioBuffer => {
